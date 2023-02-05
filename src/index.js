2909610b1a1c25464579c5fefec4ddbea49f68a7
DhanushM12 <dhanushmukhi@gmail.com>
2023-02-05 15:50:26 +0530

    Added hoc and render props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import HocCounter from './HocCounter';
+import Counter from './Counter';
+import CounterRender from './CounterRender';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// hoc
+// const Enhanced = HocCounter(Counter)
+// root.render(<Enhanced />)
+// renderprops
+root.render(<CounterRender render={props => <Counter {...props}/>} />)
